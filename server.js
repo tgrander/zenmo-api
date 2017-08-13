67b6c975eb623268d0ba0c65e8d49177779ee9ef
Trey Granderson <treygranderson@gmail.com>
2017-08-13 18:06:33 -0400

    remove es6 from server.js entirely

--- server.js
+++ server.js
@@ -3,19 +3,7 @@
 
 const app = require('./app')
 
-
 const port = process.env.PORT || 8000;
 
-(async () => {
-
-  // try {
-  //   const info = await connectDatabase();
-  //   console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
-  // } catch (error) {
-  //   console.error('Unable to connect to database');
-  //   process.exit(1);
-  // }
-
-  await app.listen(port);
-  console.log(`Server started on port ${port}`);
-})();
+app.listen(port);
+console.log(`Server started on port ${port}`);
