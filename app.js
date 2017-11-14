ec80dc3d6d54458a77c795163294999a552b83f1
tgrander <treygranderson@gmail.com>
2017-11-13 22:25:12 -0500

    cleanup app.js

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import graphqlHTTP from 'express-graphql';
 import categoriesRouter from './modules/categories/router';
 import plaidRouter from './modules/plaid/router';
 import transactionsRouter from './modules/transactions/router';
-import connectDatabase from './db';
-import { database } from './firebase';
 
 
 const app = express();
 
-
 if (!process.env.NODE_ENV) {
     throw new Error('NODE_ENV is undefined');
 }
-
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use('/categories', categoriesRouter);
 app.use('/plaid', plaidRouter);
 app.use('/transactions', transactionsRouter);
-
-
-// app.use('/graphql', graphqlHTTP({
-//     schema,
-//     graphiql: true
-// }))
-
 
 export default app;
